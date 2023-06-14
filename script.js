02f611720b7769165a3cd45dbd49644ecf07a91c
onlinity <116060268+onlinity@users.noreply.github.com>
2023-06-14 13:34:48 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -5,6 +5,13 @@ const register = document.getElementById('register')
 const newTask = document.getElementById('new')
 const addSection = document.getElementById('addSection')
 
+welcome.addEventListener('input', adjustWidth)
+
+welcome.onblur = () => {
+    localStorage.removeItem('username')
+    localStorage.setItem('username', welcome.value)
+    welcome.value = localStorage.getItem('username')
+}
 
 newTask.onclick = () => {
     addSection.classList.toggle('display')
@@ -19,7 +26,8 @@ if (localStorage.getItem('username') == null){
             localStorage.setItem('username', input)
             register.classList.add('closed')
             let username = localStorage.getItem('username')
-            welcome.innerHTML = 'Welcome ' + username
+            welcome.value = username
+            adjustWidth()
             setTimeout(() => {
                 register.style.display = 'none'
             }, 1500);
@@ -27,22 +35,39 @@ if (localStorage.getItem('username') == null){
     }
 } else{
     let username = localStorage.getItem('username')
-    welcome.innerHTML = 'Welcome ' + username
+    welcome.value = username
+    adjustWidth()
 }
 
 submit.onclick = () => {
     let text = add.value
     localStorage.setItem(Number(maxStorage()) + 1, text)
     refresh()
+    newTask.click()
     add.value = ''
+}
+
+function preventDefault(e){
+    e.preventDefault()
+}
+
+function adjustWidth() {
+    var value = welcome.value;
+    var width = value.length * 16; // 8px per character
+    welcome.style.width = width + "px";
 }
 
 function maxStorage(){
     max = 1
     for (let a = 0; a < localStorage.length; a++){
-        if (localStorage.key(a) !== 'username'){
+        if (localStorage.key(a) !== 'username' && localStorage.key(a).includes('-finished') == false){
             if (Number(localStorage.key(a)) > max){
                 max = localStorage.key(a)
+            }
+        }
+        else if (localStorage.key(a) !== 'username' && localStorage.key(a).includes('-finished')){
+            if (Number(localStorage.key(a).replace('-finished', '')) > max){
+                max = Number(localStorage.key(a).replace('-finished', ''))
             }
         }
     }
@@ -50,17 +75,26 @@ function maxStorage(){
 }
 
 refresh()
+refreshFinished()
 
 function refresh(){
     remove(document.getElementById('list'))
-    for (let i = 1; i <= Number(maxStorage()); i++){
+    for (let i = 0; i <= Number(maxStorage()); i++){
         if (localStorage.getItem(i) !== null){
             n = true
             let element = document.createElement('input')
             element.type = 'checkbox'
             element.id = 'el' + i.toString()
             let label = document.createElement('td')
-            label.innerText = localStorage.getItem(i)
+            label.id = 'tableData' + i.toString()
+            let input = document.createElement('textarea')
+            label.appendChild(input)
+            input.onblur = () => {
+                localStorage.removeItem(i)
+                localStorage.setItem(i, input.value)
+                input.value = localStorage.getItem(i)
+            }
+            input.value = localStorage.getItem(i)
             let row = document.createElement('tr')
             row.id = 'row' + i.toString()
             document.getElementById('list').appendChild(row)
@@ -71,22 +105,63 @@ function refresh(){
                 row.classList.add('checked')
                 setTimeout(() => {
                     row.style.display = 'none'
-                    finished()
                 }, 1000);
+                let date = new Date()
+                let finishedValue = localStorage.getItem(i) + ' (' + date.getDate() + '. ' + date.getMonth() + '. ' + date.getFullYear()+ '.'+ ')'
+                let finishedKey = i.toString() + '-finished'
                 localStorage.removeItem(i)
+                localStorage.setItem(finishedKey, finishedValue)
+                refreshFinished()
+                finished()
             })
         }
     }
     finished()
 }
 
+
+
+
+function refreshFinished(){
+    remove(document.getElementById('finishedTasksList'))
+    for (let i = 0; i <= localStorage.length; i++){
+        let key = localStorage.key(i)
+        if (localStorage.getItem(key) !== null && key.includes('-finished')){
+            let element = document.createElement('li')
+            element.innerHTML = localStorage.getItem(key)
+            document.getElementById('finishedTasksList').appendChild(element)
+        }
+    }
+    if (document.getElementById('finishedTasksList').innerHTML == ''){
+        document.getElementById('finishedTasks').style.display = 'none'
+    } else{
+        document.getElementById('finishedTasks').style.display = 'block'
+    }
+}
+
+
+
+function allFinished(){
+    r = true
+    for (let i = 0; i < localStorage.length; i++){
+        if (localStorage.key(i) !== 'username' && localStorage.key(i).includes('-finished') == false){
+            r = false
+        }
+    }
+    return r
+}
+
+
+
 function finished(){
-    if (localStorage.length < 2){
+    if (allFinished()){
         document.getElementById('finished').style.display = 'flex'
     } else{
         document.getElementById('finished').style.display = 'none'
     }
 }
+
+
 
 
 function remove(parent) {
